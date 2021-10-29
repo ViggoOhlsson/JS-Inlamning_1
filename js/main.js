238a3c73a07e4e3fbf57034f3e73494a43a37c79
ViggoOhlsson <viggo.e.g.o@gmail.com>
2021-10-29 12:00:36 +0200

    project started

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
+class Person {
+    constructor (name, age, isMarried) {
+        this.name = name;
+        this.age = age;
+        this.isMarried = isMarried;
+        this.asJSON = JSON.stringify(this);
+        people.push(this);
+    }
+}
+let people = [];
+let viggo = new Person("Viggo", 19, false);
 window.onload = function () {
     Main();
+    console.log(viggo.asJSON);
+    localStorage.setItem("Viggo", viggo.asJSON);
+    let result = JSON.parse(localStorage.getItem("Viggo"));
+    console.log(result);
 }
+
 function Main() {
     
 }
