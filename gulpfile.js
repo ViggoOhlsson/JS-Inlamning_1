9422bd5e1220ab5bae53ce2a2d1185da3a90162f
ViggoOhlsson <viggo.e.g.o@gmail.com>
2021-11-02 10:48:26 +0100

    gamer

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+const minify = require("gulp-minify");
 
 gulp.task('compile', function() {
     return gulp.src('scss/main.scss').pipe(sass()).pipe(gulp.dest('css'))
 });
 
+// gulp.task('minifyjs', function() {
+//     return src('js/*.js', { allowEmpty: true }) 
+//         .pipe(minify({noSource: true}))
+//         .pipe(dest('minjs'))
+// });
+
 gulp.task('watch-scss', function() {
     gulp.watch("scss/main.scss", gulp.series('compile'));
 });
 
-gulp.task('default', gulp.series('compile','watch-scss'), function(){});
+gulp.task('default', gulp.series('compile', 'watch-scss'), function(){});
 
 //install order
 // npm install -global gulp-cli
--- js/main.js
+++ js/main.js
@@ -1,22 +1,87 @@
-class Person {
-    constructor (name, age, isMarried) {
-        this.name = name;
-        this.age = age;
-        this.isMarried = isMarried;
-        this.asJSON = JSON.stringify(this);
-        people.push(this);
+class Task {
+    constructor (desc, priority) {
+        this.desc = desc;
+        this.priority = priority;
+        this.startedAt;
+        this.finishBy;
+        this.finished = false;
     }
 }
-let people = [];
-let viggo = new Person("Viggo", 19, false);
+let tasks = JSON.parse(localStorage.getItem("tasks"));
+let task1 = new Task("Make The Bed", 1);
+tasks = [task1];
+
+
 window.onload = function () {
     Main();
-    console.log(viggo.asJSON);
-    localStorage.setItem("Viggo", viggo.asJSON);
-    let result = JSON.parse(localStorage.getItem("Viggo"));
-    console.log(result);
 }
 
 function Main() {
+    console.log(tasks);
+    refreshTasks();
+
+
+    let d = new Date();
+    let t = d.toLocaleTimeString();
+    document.getElementById("watch").innerHTML = t;
+    setInterval(watchFunction, 1000);
+    
+}
+
+function watchFunction() {
+    let watch = document.getElementById("watch");
+    let d = new Date();
+    let t = d.toLocaleTimeString();
+    watch.innerHTML = t;
+}
+
+function refreshTasks() {
+    let activeTaskList = document.getElementById("active-list");
+    let finishedTaskList = document.getElementById("finished-list");
+    for (i = 0; i > tasks.length; i++) {
+        let listRow = document.createElement("li");
+        listRow.id = "task" + i;
+        let todoButton = documnet.createElement("button");
+        todoButton.className = "todo-button";
+        todoButton.innerHTML = "<i class='fa fa-check'></i>";
+        listRow.appendChild(todoButton);
+        let todoTitle = document.createElement("span");
+        todoTitle.className = "todo-title";
+        todoTitle.className = tasks[i].desc;
+        listRow.appendChild(todoTitle);
+        let todoStarted = document.createElement("span");
+        todoStarted.className = "todo-started";
+        todoStarted.innerHTML = "Started At <span>" + 123 + "</span>";
+        listRow.appendChild(todoStarted);
+        let todoFinishBy = document.createElement("span");
+        todoFinishBy.className = "todo-finishby";
+        todoFinishBy.innerHTML = "Finish By <span>" + 123 + "</span>";
+        listRow.appendChild(todoFinishBy);
+        let priority;
+        switch (tasks[i].priority) {
+            case 1: {
+                priority = "Priority <span class='prio-1'>Top</span>";
+                break;
+            }
+            case 2: {
+                priority = "Priority <span class='prio-2'>Mid</span>";
+                break;
+            }
+            case 3: {
+                priority = "Priority <span class='prio-3'>Low</span>";
+                break;
+            }
+            default:
+                priority = "Priority <span class='prio-3'>Low</span>";
+                break;
+        }
+
+        let todoPrio = document.createElement("span");
+        todoPrio.className = "todo-prio"
+        todoPrio.innerHTML = priority;
+        listRow.appendChild(todoPrio);
+    }
+    
+
     
 }
